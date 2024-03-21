1ca553f3fbe07eec8a21c29f85da9a66dae23e60
rhowelp <55319885+rhowelp@users.noreply.github.com>
2024-03-21 08:20:51 +0800

    fix: connecting to websocket

--- lib/features/home_view/home_view.dart
+++ lib/features/home_view/home_view.dart
@@ -43,9 +43,9 @@ class _HomePageViewState extends State<HomePageView> {
       try {
         await _channel!.ready;
       } on SocketException catch (e) {
-        print('=====> SocketException $e');
+        debugPrint('=====> SocketException $e');
       } on WebSocketChannelException catch (e) {
-        print('=====> WebSocketChannelException $e');
+        debugPrint('=====> WebSocketChannelException $e');
       }
       _channel!.stream.listen(
         (message) {
@@ -55,18 +55,34 @@ class _HomePageViewState extends State<HomePageView> {
             body: notification['message'],
           );
 
-          print('message=====> Received message: $message');
+          debugPrint('message=====> Received message: $message');
         },
         onDone: () {
-          print('onDone=====> Connection Closed');
+          debugPrint('onDone=====> Connection Closed');
         },
         onError: (error) {
-          print('onError=====> ERROR $error');
+          debugPrint('onError=====> ERROR $error');
         },
       );
     } catch (e) {
-      print('=====> Error connecting to WebSocket: $e');
+      debugPrint('=====> Error connecting to WebSocket: $e');
     }
+  }
+
+  Future<void> getCookiesData(WebUri url) async {
+    final cookies = await _cookieManager.getCookies(url: url);
+    // debugPrint('COOKIE ${cookies.map((e) => e.toJson()).toList()}');
+    final sessionId = cookies.firstWhere((cookie) => cookie.name == 'sessionId').value;
+
+    setState(() {
+      if (sessionId != null && !_isConnected) {
+        // Connect to WebSocket with sessionId token
+        debugPrint('=======> Connecting to WebSocket...');
+        connectToWebSocket(sessionId);
+      }
+      _isConnected = true;
+      debugPrint('=======> Connected: $_isConnected');
+    });
   }
 
   @override
@@ -85,7 +101,6 @@ class _HomePageViewState extends State<HomePageView> {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.arrow_back),
-            // ignore: unnecessary_lambdas
             onPressed: () {
               if (webViewController != null) {
                 webViewController!.goBack();
@@ -135,45 +150,35 @@ class _HomePageViewState extends State<HomePageView> {
               },
               onLoadStart: (controller, url) {
                 setState(() {
-                  print('=======> Page started loading: $url');
+                  debugPrint('=======> Page started loading: $url');
                 });
               },
               shouldOverrideUrlLoading: (controller, navigationAction) async {
                 var uri = navigationAction.request.url;
-                print('URI $uri');
+                debugPrint('URI $uri');
                 return NavigationActionPolicy.ALLOW;
-              },
-              onUpdateVisitedHistory: (controller, url, isReload) {
-                webViewController!.loadUrl(urlRequest: URLRequest(url: url));
-                isReload = true;
               },
               onNavigationResponse: (controller, navigationResponse) async {
                 return NavigationResponseAction.ALLOW;
               },
               onLoadStop: (controller, url) async {
-                print('=======> Page onLoadStop: $url');
-                final cookies = await _cookieManager.getCookies(url: url!);
-                print('COOKIE ${cookies.map((e) => e.toJson()).toList()}');
-                final sessionId = cookies.firstWhere((cookie) => cookie.name == 'sessionId').value;
-
-                setState(() {
-                  print('=======> Page finished loading: $url');
-
-                  if (sessionId != null && !_isConnected) {
-                    // Connect to WebSocket with sessionId token
-                    print('=======> Connecting to WebSocket...');
-                    connectToWebSocket(sessionId);
-                  }
-                  print('=======>  $_isConnected');
-                });
+                debugPrint('=======> Page onLoadStop: $url');
+                getCookiesData(url!);
               },
               onProgressChanged: (InAppWebViewController controller, int progress) {
                 setState(() {
                   this.progress = progress / 100;
                 });
               },
-              onConsoleMessage: (controller, consoleMessage) {
-                print('=======> CONSOLE ${consoleMessage.message}');
+              onConsoleMessage: (controller, consoleMessage) async {
+                if (consoleMessage.message == 'WebSocket Open') {
+                  final url = await controller.getUrl();
+                  getCookiesData(url!);
+                } else {
+                  if (_channel != null) {
+                    _channel!.sink.close();
+                  }
+                }
               },
             ),
           ),
@@ -222,245 +227,3 @@ class _HomePageViewState extends State<HomePageView> {
     }
   }
 }
-
-// import 'dart:convert';
-// import 'dart:io';
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-// import 'package:tinselcrm_mobile/core/local_notification/local_notification.dart';
-// import 'package:web_socket_channel/io.dart';
-// import 'package:web_socket_channel/web_socket_channel.dart';
-// import 'package:webview_flutter/webview_flutter.dart';
-
-// class HomePageView extends StatefulWidget {
-//   const HomePageView({super.key});
-
-//   @override
-//   State<HomePageView> createState() => _HomePageViewState();
-// }
-
-// class _HomePageViewState extends State<HomePageView> {
-//   WebSocketChannel? _channel;
-//   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-//   final localNotification = NotificationPlugin();
-//   late final WebViewController _controller;
-//   late final PlatformWebViewControllerCreationParams params;
-//   final _cookieManager = CookieManager.instance();
-//   bool _isConnected = false;
-//   double progress = 0;
-
-//   Future<void> getUnionBank() async {
-//     try {
-//       await _controller.enableZoom(true);
-//       await _controller.setJavaScriptMode(JavaScriptMode.unrestricted);
-//       await _controller.setBackgroundColor(Colors.green.withOpacity(0.3));
-//       await _controller.addJavaScriptChannel('Print', onMessageReceived: (logs) {
-//         print('CONSOLE ${logs.message}');
-//       });
-//       await _controller.setOnConsoleMessage((logs) {
-//         print('CONSOLE2 ${logs.message}');
-//       });
-//       await _controller.((logs) {
-//         print('CONSOLE2 ${logs.message}');
-//       });
-//       await _controller.setNavigationDelegate(
-//         NavigationDelegate(
-//           onUrlChange: (change) {
-//             print("CHANGED ${change.url}");
-//             final url = Uri.parse(change.url!);
-//             if (url.pathSegments.isNotEmpty) {
-//               if (url.pathSegments.first == 'login') {
-//                 if (_channel != null) {
-//                   _channel!.sink.close();
-//                   print('onDone=====> Connection Closed');
-//                 }
-//               }
-//             }
-//           },
-//           onProgress: (int progress) {
-//             setState(() {
-//               this.progress = progress / 100;
-//             });
-//             print('=====================> onProgress $progress');
-//           },
-//           onPageStarted: (String url) {
-//             print('=====================> onPageStarted');
-//           },
-//           onPageFinished: (String url) async {
-//             final cookies = await _cookieManager.getCookies(url: WebUri(url));
-//             print('COOKIE ${cookies.map((e) => e.toJson()).toList()}');
-//             final sessionId = cookies.firstWhere((cookie) => cookie.name == 'sessionId').value;
-//             print('sessionId $sessionId');
-//             setState(() {
-//               print('Page finished loading: $url');
-
-//               if (sessionId != null && !_isConnected) {
-//                 // Connect to WebSocket with sessionId token
-//                 print('Connecting to WebSocket...');
-//                 connectToWebSocket(sessionId);
-//               }
-//               print('_isConnected $_isConnected');
-//             });
-//           },
-//           onWebResourceError: (error) {
-//             debugPrint(
-//               '''
-//                   ERROR:
-//                   code: ${error.errorCode}
-//                   description ${error.description}
-//                   errorType: ${error.errorType}
-//                   isForMainFrame: ${error.isForMainFrame}
-//                 ''',
-//             );
-//           },
-//           onNavigationRequest: (request) {
-//             if (request.url.startsWith('https://')) {
-//               return NavigationDecision.navigate;
-//             } else {
-//               return NavigationDecision.prevent;
-//             }
-//           },
-//         ),
-//       );
-
-//       await _controller.loadRequest(Uri.parse('https://dev2.tinselcrm.com/login'));
-//       // final String cookies = await _controller.ja('document.cookie');
-//     } catch (e) {
-//       debugPrint('Error $e');
-//       throw Exception(e);
-//     }
-//   }
-
-//   void connectToWebSocket(String sessionId) async {
-//     try {
-//       _channel = IOWebSocketChannel.connect(
-//         'wss://dev2.tinselcrm.com/webSocketServer',
-//         headers: {
-//           'Cookie': 'sessionId=$sessionId',
-//         },
-//       );
-//       try {
-//         await _channel!.ready;
-//       } on SocketException catch (e) {
-//         print('=====> SocketException $e');
-//       } on WebSocketChannelException catch (e) {
-//         print('=====> WebSocketChannelException $e');
-//       }
-//       _channel!.stream.listen(
-//         (message) {
-//           final notification = json.decode(message) as Map<String, dynamic>;
-//           localNotification.showNotification(
-//             title: notification['title'],
-//             body: notification['message'],
-//           );
-
-//           print('=====> Received message: $message');
-//         },
-//         onDone: () {
-//           print('onDone=====> Connection Closed');
-//         },
-//         onError: (error) {
-//           print('onError=====> ERROR $error');
-//         },
-//       );
-//     } catch (e) {
-//       print('=====> Error connecting to WebSocket: $e');
-//     }
-//   }
-
-//   @override
-//   initState() {
-//     super.initState();
-//     _controller = WebViewController(); 
-//     getUnionBank();
-//     _requestPermissions();
-//     _isAndroidPermissionGranted();
-//     localNotification.initializeNotifications();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         actions: <Widget>[
-//           IconButton(
-//             icon: const Icon(Icons.arrow_back),
-//             onPressed: () async {
-//               _controller.goBack();
-//             },
-//           ),
-//           IconButton(
-//             icon: const Icon(Icons.arrow_forward),
-//             onPressed: () {
-//               _controller.goForward();
-//             },
-//           ),
-//           IconButton(
-//             icon: const Icon(Icons.refresh),
-//             onPressed: () {
-//               _controller.reload();
-//             },
-//           ),
-//         ],
-//       ),
-//       body: SafeArea(
-//         child: Column(
-//           children: [
-//             Container(
-//               child: progress < 1.0
-//                   ? Padding(
-//                       padding: const EdgeInsets.all(2),
-//                       child: LinearProgressIndicator(value: progress),
-//                     )
-//                   : Container(),
-//             ),
-//             Expanded(child: WebViewWidget(controller: _controller)),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   bool notificationsEnabled = false;
-//   Future<void> _requestPermissions() async {
-//     if (Platform.isIOS || Platform.isMacOS) {
-//       await flutterLocalNotificationsPlugin
-//           .resolvePlatformSpecificImplementation<IOSFlutterLocalNotificationsPlugin>()
-//           ?.requestPermissions(
-//             alert: true,
-//             badge: true,
-//             sound: true,
-//           );
-//       await flutterLocalNotificationsPlugin
-//           .resolvePlatformSpecificImplementation<MacOSFlutterLocalNotificationsPlugin>()
-//           ?.requestPermissions(
-//             alert: true,
-//             badge: true,
-//             sound: true,
-//           );
-//     } else if (Platform.isAndroid) {
-//       final AndroidFlutterLocalNotificationsPlugin? androidImplementation = flutterLocalNotificationsPlugin
-//           .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>();
-
-//       final bool? grantedNotificationPermission = await androidImplementation?.requestNotificationsPermission();
-//       setState(() {
-//         notificationsEnabled = grantedNotificationPermission ?? false;
-//       });
-//     }
-//   }
-
-//   Future<void> _isAndroidPermissionGranted() async {
-//     if (Platform.isAndroid) {
-//       final bool granted = await flutterLocalNotificationsPlugin
-//               .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
-//               ?.areNotificationsEnabled() ??
-//           false;
-
-//       setState(() {
-//         notificationsEnabled = granted;
-//       });
-//     }
-//   }
-// }
