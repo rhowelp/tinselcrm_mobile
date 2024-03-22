b11a2feb335124f3365393d11ec8957914e8643d
rhowelp <55319885+rhowelp@users.noreply.github.com>
2024-03-23 03:35:17 +0800

    fix: enable url change and new ui design

--- lib/features/home_view/home_view.dart
+++ lib/features/home_view/home_view.dart
@@ -1,15 +1,11 @@
-// ignore_for_file: use_build_context_synchronously, lines_longer_than_80_chars, avoid_print
-
-import 'dart:async';
-import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:tinselcrm_mobile/core/local_notification/local_notification.dart';
-import 'package:tinselcrm_mobile/main.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
@@ -23,10 +19,16 @@ class HomePageView extends StatefulWidget {
 class _HomePageViewState extends State<HomePageView> {
   final GlobalKey webViewKey = GlobalKey();
   WebSocketChannel? _channel;
+  final _urlController = TextEditingController();
+  final _urlFocusNode = FocusNode();
+  String _currentUrl = '';
+  String title = '';
+  double progress = 0;
+  bool? isSecure;
+  bool isUrlUpdating = false;
   InAppWebViewController? webViewController;
   final _cookieManager = CookieManager.instance();
   bool _isConnected = false;
-  double progress = 0;
 
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
   final localNotification = NotificationPlugin();
@@ -88,84 +90,189 @@ class _HomePageViewState extends State<HomePageView> {
   @override
   initState() {
     super.initState();
+    _currentUrl = 'https://app.tinselcrm.com/login';
     _isAndroidPermissionGranted();
     _requestPermissions();
     localNotification.initializeNotifications();
   }
 
   final webStorageManager = WebStorageManager.instance();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              if (webViewController != null) {
-                webViewController!.goBack();
-              }
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.arrow_forward),
-            onPressed: () {
-              if (webViewController != null) {
-                webViewController!.goForward();
-              }
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            onPressed: () {
-              if (webViewController != null) {
-                webViewController!.reload();
-              }
-            },
-          ),
-        ],
+        title: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FutureBuilder<bool>(
+              future: webViewController?.canGoBack() ?? Future.value(false),
+              builder: (context, snapshot) {
+                final canGoBack = snapshot.hasData ? snapshot.data! : false;
+                return IconButton(
+                  icon: const Icon(Icons.chevron_left),
+                  onPressed: !canGoBack
+                      ? null
+                      : () {
+                          webViewController?.goBack();
+                        },
+                );
+              },
+            ),
+            FutureBuilder<bool>(
+              future: webViewController?.canGoForward() ?? Future.value(false),
+              builder: (context, snapshot) {
+                final canGoForward = snapshot.hasData ? snapshot.data! : false;
+                return IconButton(
+                  icon: const Icon(Icons.chevron_right),
+                  onPressed: !canGoForward
+                      ? null
+                      : () {
+                          webViewController?.goForward();
+                        },
+                );
+              },
+            ),
+            Expanded(
+              child: isUrlUpdating
+                  ? TextFormField(
+                      controller: _urlController,
+                      decoration: InputDecoration(
+                        hintText: 'Enter URL',
+                        isDense: true,
+                        contentPadding: EdgeInsets.all(10),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+                      ),
+                      focusNode: _urlFocusNode,
+                      onTapOutside: (event) {
+                        _urlFocusNode.unfocus();
+                        setState(() {
+                          isUrlUpdating = false;
+                        });
+                      },
+                      onFieldSubmitted: (url) {
+                        if (WebUri(url).isValidUri) {
+                          print("SUBMITTED");
+                          _urlFocusNode.unfocus();
+                          setState(() {
+                            isUrlUpdating = false;
+                            _urlController.text = url;
+                          });
+
+                          webViewController!.loadUrl(urlRequest: URLRequest(url: WebUri(url)));
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Not a valid URL.'),
+                            ),
+                          );
+                        }
+                      },
+                    )
+                  : GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _urlFocusNode.requestFocus();
+                          isUrlUpdating = true;
+                        });
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            title,
+                            overflow: TextOverflow.fade,
+                          ),
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              isSecure != null
+                                  ? Icon(
+                                      isSecure == true ? Icons.lock : Icons.lock_open,
+                                      color: isSecure == true ? Colors.green : Colors.red,
+                                      size: 12,
+                                    )
+                                  : Container(),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Flexible(
+                                child: Text(
+                                  _currentUrl,
+                                  style: const TextStyle(fontSize: 12, color: Colors.white70),
+                                  overflow: TextOverflow.fade,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: () {
+                webViewController?.reload();
+              },
+            ),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          Container(
-            child: progress < 1.0
-                ? Padding(
-                    padding: const EdgeInsets.all(2),
-                    child: LinearProgressIndicator(value: progress),
-                  )
-                : Container(),
-          ),
-          Expanded(
-            child: InAppWebView(
+      body: Column(children: <Widget>[
+        Expanded(
+            child: Stack(
+          children: [
+            InAppWebView(
               key: webViewKey,
-              initialUserScripts: UnmodifiableListView(userScripts),
+              initialUrlRequest: URLRequest(url: WebUri(_currentUrl)),
               initialSettings: InAppWebViewSettings(
-                useShouldOverrideUrlLoading: true,
-                mediaPlaybackRequiresUserGesture: false,
-                allowsInlineMediaPlayback: true,
-              ),
-              initialUrlRequest: URLRequest(url: WebUri.uri(Uri.parse('https://app.tinselcrm.com/'))),
-              onWebViewCreated: (InAppWebViewController controller) {
+                  transparentBackground: true, safeBrowsingEnabled: true, isFraudulentWebsiteWarningEnabled: true),
+              onWebViewCreated: (controller) async {
                 webViewController = controller;
+                if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+                  await controller.startSafeBrowsing();
+                }
               },
               onLoadStart: (controller, url) {
-                setState(() {
-                  debugPrint('=======> Page started loading: $url');
-                });
-              },
-              shouldOverrideUrlLoading: (controller, navigationAction) async {
-                var uri = navigationAction.request.url;
-                debugPrint('URI $uri');
-                return NavigationActionPolicy.ALLOW;
-              },
-              onNavigationResponse: (controller, navigationResponse) async {
-                return NavigationResponseAction.ALLOW;
+                if (url != null) {
+                  setState(() {
+                    this._currentUrl = url.toString();
+                    isSecure = urlIsSecure(url);
+                  });
+                }
               },
               onLoadStop: (controller, url) async {
+                if (url != null) {
+                  setState(() {
+                    this._currentUrl = url.toString();
+                  });
+                }
+
                 debugPrint('=======> Page onLoadStop: $url');
                 getCookiesData(url!);
+
+                final sslCertificate = await controller.getCertificate();
+                setState(() {
+                  isSecure = sslCertificate != null || (urlIsSecure(url));
+                });
               },
-              onProgressChanged: (InAppWebViewController controller, int progress) {
+              onUpdateVisitedHistory: (controller, url, isReload) {
+                if (url != null) {
+                  setState(() {
+                    this._currentUrl = url.toString();
+                  });
+                }
+              },
+              onTitleChanged: (controller, title) {
+                if (title != null) {
+                  setState(() {
+                    this.title = title;
+                  });
+                }
+              },
+              onProgressChanged: (controller, progress) {
                 setState(() {
                   this.progress = progress / 100;
                 });
@@ -180,11 +287,115 @@ class _HomePageViewState extends State<HomePageView> {
                   }
                 }
               },
+              shouldOverrideUrlLoading: (controller, navigationAction) async {
+                final url = navigationAction.request.url;
+                if (navigationAction.isForMainFrame &&
+                    url != null &&
+                    !['http', 'https', 'file', 'chrome', 'data', 'javascript', 'about'].contains(url.scheme)) {
+                  // if (await canLaunchUrl(url)) {
+                  //   launchUrl(url);
+                  //   return NavigationActionPolicy.CANCEL;
+                  // }
+                }
+                return NavigationActionPolicy.ALLOW;
+              },
             ),
-          ),
-        ],
-      ),
+            progress < 1.0 ? LinearProgressIndicator(value: progress) : Container(),
+          ],
+        )),
+      ]),
+      // bottomNavigationBar: BottomAppBar(
+      //   child: Row(
+      //     mainAxisSize: MainAxisSize.max,
+      //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     children: <Widget>[
+      //       IconButton(
+      //         icon: const Icon(Icons.refresh),
+      //         onPressed: () {
+      //           webViewController?.reload();
+      //         },
+      //       ),
+      //       PopupMenuButton<int>(
+      //         onSelected: (item) => handleClick(item),
+      //         itemBuilder: (context) => [
+      //           // PopupMenuItem<int>(
+      //           //   enabled: false,
+      //           //   child: Column(
+      //           //     children: [
+      //           //       Row(
+      //           //         children: const [
+      //           //           FlutterLogo(),
+      //           //           Expanded(
+      //           //             child: Center(
+      //           //               child: Text(
+      //           //                 'Other options',
+      //           //                 style: TextStyle(color: Colors.black),
+      //           //               ),
+      //           //             ),
+      //           //           ),
+      //           //         ],
+      //           //       ),
+      //           //       const Divider()
+      //           //     ],
+      //           //   ),
+      //           // ),
+      //           PopupMenuItem<int>(
+      //             value: 0,
+      //             child: Row(
+      //               children: const [
+      //                 Icon(Icons.open_in_browser),
+      //                 SizedBox(
+      //                   width: 5,
+      //                 ),
+      //                 Text('Open in the Browser')
+      //               ],
+      //             ),
+      //           ),
+      //           PopupMenuItem<int>(
+      //             value: 1,
+      //             child: Row(
+      //               children: const [
+      //                 Icon(Icons.clear_all),
+      //                 SizedBox(
+      //                   width: 5,
+      //                 ),
+      //                 Text('Clear your browsing data')
+      //               ],
+      //             ),
+      //           ),
+      //         ],
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
+  }
+
+  void handleClick(int item) async {
+    switch (item) {
+      case 0:
+        await InAppBrowser.openWithSystemBrowser(url: WebUri(_currentUrl));
+        break;
+      case 1:
+        await InAppWebViewController.clearAllCache();
+        if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+          await webViewController?.clearHistory();
+        }
+        setState(() {});
+        break;
+    }
+  }
+
+  static bool urlIsSecure(Uri url) {
+    return (url.scheme == "https") || isLocalizedContent(url);
+  }
+
+  static bool isLocalizedContent(Uri url) {
+    return (url.scheme == "file" ||
+        url.scheme == "chrome" ||
+        url.scheme == "data" ||
+        url.scheme == "javascript" ||
+        url.scheme == "about");
   }
 
   Future<void> _requestPermissions() async {
