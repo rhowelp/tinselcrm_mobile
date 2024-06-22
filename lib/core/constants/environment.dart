a0a74914065beab88ddff370284020b7a8aa37d3
rhowelp <55319885+rhowelp@users.noreply.github.com>
2024-06-22 22:06:39 +0800

    added environment file

---
+++ lib/core/constants/environment.dart
@@ -0,0 +1,6 @@
+
+class Environment {
+  static const String API_HOST_PROD = 'https://api.tinselcrm.com';
+  static const String API_HOST_DEV = 'https://dev2.tinselcrm.com';
+  static const String API_PATH = '/api';
+}
--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,10 +1,11 @@
 import 'package:chopper/chopper.dart';
 import 'package:flutter/foundation.dart';
+import 'package:tinselcrm_mobile/core/constants/environment.dart';
 import 'package:tinselcrm_mobile/core/interceptors/token_interceptor.dart';
 
 part 'api_service.chopper.dart';
 
-@ChopperApi(baseUrl: 'https://dev2.tinselcrm.com/api')
+@ChopperApi(baseUrl: '${Environment.API_HOST_DEV}/${Environment.API_PATH}')
 abstract class ApiService extends ChopperService {
   static ApiService create() {
     final client = ChopperClient(
